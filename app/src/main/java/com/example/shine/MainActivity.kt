95d3fb8ac81c57275b0e804b25f5027e21646c1c
Yurec <dreamyinpolar@gmail.com>
2022-12-14 15:48:26 +0400

    added fragment container

--- app/src/main/java/com/example/shine/MainActivity.kt
+++ app/src/main/java/com/example/shine/MainActivity.kt
@@ -3,16 +3,16 @@ package com.example.shine
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.add
+import androidx.fragment.app.commit
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        supportFragmentManager.commit {
+            setReorderingAllowed(true)
+            add<PlaylistFragment>(R.id.fragment_container)
+        }
     }
-
-
-
-
 }
-
-
---
+++ app/src/main/java/com/example/shine/PlaylistFragment.kt
@@ -0,0 +1,8 @@
+package com.example.shine
+
+import androidx.fragment.app.Fragment
+
+class PlaylistFragment : Fragment(R.layout.fragment_playlist) {
+
+
+}
