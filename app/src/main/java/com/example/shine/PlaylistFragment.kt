546d805bee344a40eaf5e2ae489052f83079ff83
Yurec <dreamyinpolar@gmail.com>
2022-12-15 18:03:26 +0400

    added simple recycler view

--- app/src/main/java/com/example/shine/PlaylistFragment.kt
+++ app/src/main/java/com/example/shine/PlaylistFragment.kt
@@ -1,8 +1,34 @@
 package com.example.shine
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
 
 class PlaylistFragment : Fragment(R.layout.fragment_playlist) {
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        val recycler = view.findViewById<RecyclerView>(R.id.recyclerView)
+        val adapter = SongAdapter()
+        recycler.adapter = adapter
+        adapter.updateSongs(getSongs())
+    }
+
+    fun getSongs(): List<Song> {
+        return listOf(
+            Song(title = "favorite", description = "wadaweaw"),
+            Song(title = "favorite", description = "awd"),
+            Song(title = "favorite", description = "awd"),
+            Song("favorite", "waascdaweaw"),
+            Song("favorite", "daxa"),
+            Song("favorite", "wadascweaw"),
+            Song("favorite", "wadascsweaw"),
+            Song("favorite", "wawedaweaw"),
+            Song("favorite", "wadawdewweaw"),
+            Song("favorite", "wadaxzcweaw"),
+            Song("favorite", "wadaxzczweaw"),
+            )
+    }
 }
